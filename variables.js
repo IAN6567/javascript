295d4c64b38d6fabac05f2b35a6ed527de46a692
Ian kipngetich <iankipngetich791@gmail.com>
2025-07-09 12:04:01 +0300

    done

---
+++ variables.js
@@ -0,0 +1,17 @@
+//variables are used to store data that can be used throughout the application.
+//they can be used to store configuration settings user preferences or any other data that need to be accessed globally
+
+//keywords in programming languages are reserved words that have spexial meaning in the language.they are used to define the structure and behavuiour of the code.in javascript tkeywords use var ,let, const, function if else for while and many others
+
+//var was the original way to declare variables in javascript because of the scope issues it is now recommended to use the let and const instead
+
+var username = "ian";
+//let is used to decsribe variables that can be reasigned later.it has block scope,meaning it is only accesible within the block it is defined in.
+let age = 30;
+//const is used to declare variables that cannot be reassigned.it is also block scoped
+const pi = 3.14;
+const role = "distributor";
+let darkMole = false;
+age = 31;
+
+//pi = 3.1;
