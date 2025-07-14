136f5bd49c11deaa5ac105f9b77bc0fd201f367c
Ian kipngetich <iankipngetich791@gmail.com>
2025-07-14 09:59:19 +0300

    done

---
+++ operators.js
@@ -0,0 +1,30 @@
+// Operators in JS
+
+// operators in js are special symbols or keywords thaat perform operstiond on values or variables.They can be categorized intp several types based on their functionality. Here are some common types of operators in javaascript.
+
+// 1. Arithmetic Opeerators: Used for mathematical calculation. Example; +,-,*,/,%
+console.log(5 / 3); //modulus operator, returns the remainder of a division operation
+let daynumber = 4;
+
+if (daynumber % 4 == 0) {
+  console.log("Alert! send your cut");
+}
+
+// 2. Assignment Operaators: Used to assign values to variables. Example; =,+=,-=,*=,/=,%=.
+daynumber += 10; //daynumber ---->daynummber = daynumber+10
+console.log(daynumber);
+
+// 3. Comparison Operators: Used to compare values nd return a boolean result. Examples: ==,===,!=,!==,<,>,<=,>=.
+console.log(5 == "5"); //lloose equality checks only value
+console.log(5 === "5"); // strict quality, checks both values and type
+console.log(32 > 20);
+
+let age = 21;
+
+if (age >= 21) {
+  console.log("Allow into the website");
+}
+// 4. Logical Operators: Used to perform logical operations nd boolean values. Example; &&(AND),||(OR),!(NOT)no
+// 5.Ternary Operator: A shortened for conditional statements. cconditions? valueifTrue : valueifFalse.
+
+// The Truth Table
